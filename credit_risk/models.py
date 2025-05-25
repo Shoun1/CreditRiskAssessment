01eedde93e9e2f87ea9d785ba0ef846d770c66b1
Shounak Das <dasshounak2003@gmail.com>
2025-05-26 03:01:21 +0530

    new commit

---
+++ credit_risk/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Borrower(models.Model):
+    grade = models.CharField(max_length=1)
+    defaulter = models.CharField(max_length=1)
+    class Meta:
+        db_table = 'Borrower'
+
+class Plots(models.Model):
+    plot = models.ImageField()
